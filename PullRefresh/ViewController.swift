9efc34bcb59edb24949d8f78d4f6f79baec3f7d9
Simon Ng <simon@Simons-MacBook-Pro.local>
2015-06-11 00:18:36 +0800

    Initial version

--- PullRefresh/ViewController.swift
+++ PullRefresh/ViewController.swift
@@ -8,11 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tblDemo: UITableView!
+    
+    var dataArray: Array<String> = ["One", "Two", "Three", "Four", "Five"]
+    
+    var refreshControl: UIRefreshControl!
+    
+    var customView: UIView!
+    
+    var labelsArray: Array<UILabel> = []
+    
+    var isAnimating = false
+    
+    var currentColorIndex = 0
+    
+    var currentLabelIndex = 0
+    
+    var timer: NSTimer!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tblDemo.delegate = self
+        tblDemo.dataSource = self
+        
+        refreshControl = UIRefreshControl()
+        refreshControl.backgroundColor = UIColor.clearColor()
+        refreshControl.tintColor = UIColor.clearColor()
+        tblDemo.addSubview(refreshControl)
+        
+        loadCustomRefreshContents()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +49,149 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    // MARK: UITableview method implementation
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataArray.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("idCell", forIndexPath: indexPath) as! UITableViewCell
+        
+        cell.textLabel!.text = dataArray[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 60.0
+    }
 
+    
+    
+    // MARK: UIScrollView delegate method implementation
+    
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+        if refreshControl.refreshing {
+            if !isAnimating {
+                doSomething()
+                animateRefreshStep1()
+            }
+        }
+    }
+    
+    
+    // MARK: Custom function implementation
+    
+    func loadCustomRefreshContents() {
+        let refreshContents = NSBundle.mainBundle().loadNibNamed("RefreshContents", owner: self, options: nil)
+        
+        customView = refreshContents[0] as! UIView
+        customView.frame = refreshControl.bounds
+        
+        for var i=0; i<customView.subviews.count; ++i {
+            labelsArray.append(customView.viewWithTag(i + 1) as! UILabel)
+        }
+        
+        refreshControl.addSubview(customView)
+    }
+    
+    
+    func animateRefreshStep1() {
+        isAnimating = true
+        
+        UIView.animateWithDuration(0.1, delay: 0.0, options: UIViewAnimationOptions.CurveLinear, animations: { () -> Void in
+            self.labelsArray[self.currentLabelIndex].transform = CGAffineTransformMakeRotation(CGFloat(M_PI_4))
+            self.labelsArray[self.currentLabelIndex].textColor = self.getNextColor()
+            
+            }, completion: { (finished) -> Void in
+                
+                UIView.animateWithDuration(0.05, delay: 0.0, options: UIViewAnimationOptions.CurveLinear, animations: { () -> Void in
+                    self.labelsArray[self.currentLabelIndex].transform = CGAffineTransformIdentity
+                    self.labelsArray[self.currentLabelIndex].textColor = UIColor.blackColor()
+                    
+                    }, completion: { (finished) -> Void in
+                        ++self.currentLabelIndex
+                        
+                        if self.currentLabelIndex < self.labelsArray.count {
+                            self.animateRefreshStep1()
+                        }
+                        else {
+                            self.animateRefreshStep2()
+                        }
+                })
+        })
+    }
+    
+    
+    func animateRefreshStep2() {
+        UIView.animateWithDuration(0.35, delay: 0.0, options: UIViewAnimationOptions.CurveLinear, animations: { () -> Void in
+            self.labelsArray[0].transform = CGAffineTransformMakeScale(1.5, 1.5)
+            self.labelsArray[1].transform = CGAffineTransformMakeScale(1.5, 1.5)
+            self.labelsArray[2].transform = CGAffineTransformMakeScale(1.5, 1.5)
+            self.labelsArray[3].transform = CGAffineTransformMakeScale(1.5, 1.5)
+            self.labelsArray[4].transform = CGAffineTransformMakeScale(1.5, 1.5)
+            self.labelsArray[5].transform = CGAffineTransformMakeScale(1.5, 1.5)
+            self.labelsArray[6].transform = CGAffineTransformMakeScale(1.5, 1.5)
+            
+            }, completion: { (finished) -> Void in
+                UIView.animateWithDuration(0.25, delay: 0.0, options: UIViewAnimationOptions.CurveLinear, animations: { () -> Void in
+                    self.labelsArray[0].transform = CGAffineTransformIdentity
+                    self.labelsArray[1].transform = CGAffineTransformIdentity
+                    self.labelsArray[2].transform = CGAffineTransformIdentity
+                    self.labelsArray[3].transform = CGAffineTransformIdentity
+                    self.labelsArray[4].transform = CGAffineTransformIdentity
+                    self.labelsArray[5].transform = CGAffineTransformIdentity
+                    self.labelsArray[6].transform = CGAffineTransformIdentity
+                    
+                    }, completion: { (finished) -> Void in
+                        if self.refreshControl.refreshing {
+                            self.currentLabelIndex = 0
+                            self.animateRefreshStep1()
+                        }
+                        else {
+                            self.isAnimating = false
+                            self.currentLabelIndex = 0
+                            for var i=0; i<self.labelsArray.count; ++i {
+                                self.labelsArray[i].textColor = UIColor.blackColor()
+                                self.labelsArray[i].transform = CGAffineTransformIdentity
+                            }
+                        }
+                })
+        })
+    }
+    
+    
+    func getNextColor() -> UIColor {
+        var colorsArray: Array<UIColor> = [UIColor.magentaColor(), UIColor.brownColor(), UIColor.yellowColor(), UIColor.redColor(), UIColor.greenColor(), UIColor.blueColor(), UIColor.orangeColor()]
+        
+        if currentColorIndex == colorsArray.count {
+            currentColorIndex = 0
+        }
+        
+        let returnColor = colorsArray[currentColorIndex]
+        ++currentColorIndex
+        
+        return returnColor
+    }
+    
+    
+    func doSomething() {
+        timer = NSTimer.scheduledTimerWithTimeInterval(4.0, target: self, selector: "endOfWork", userInfo: nil, repeats: true)
+    }
+    
+    
+    func endOfWork() {
+        refreshControl.endRefreshing()
+        
+        timer.invalidate()
+        timer = nil
+    }
+    
 }
 
